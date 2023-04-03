8c0c31d843a922e58a41f685fa5e26f01a7709be
rednav1979 <102362268+rednav1979@users.noreply.github.com>
2023-04-03 13:29:32 -0300

    Update sql_t.i.php

--- sql_t.i.php
+++ sql_t.i.php
@@ -1,7 +1,7 @@
 <?php
    $host = "localhost";//caso esteja usando o xampp ou wamp
    $user = "root";
-   $pass = "C0st4sul";
+   $pass = "aaaaa";
    $db = "tecnologia";
    $conn = mysql_connect($host, $user, $pass) or die (mysql_error());
    
